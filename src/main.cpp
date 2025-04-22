e5f6f7eed0b137387b05dbd428f476aa039c0074
Mason <m.porter1@ufl.edu>
2025-04-22 13:07:50 -0400

    Data insertion, still needs hash

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include <string>
+#include <fstream>
+#include <sstream>
 #include "Trie.h"
 #include "Test.h"
 #include "GUI.h"
@@ -5,8 +8,32 @@
 using namespace std;
 
 int main() {
-    // Reading from the file and inserting into the data structure
-
+    // reading from the file and inserting into the data structure
+    // create data structure objects
+    Trie trie;
+    // (HASH OBJECT HERE) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // open file
+    string filename = "../Data/10millionPasswords.csv";
+    ifstream file;
+    file.open(filename);
+    if (!file.is_open()) {
+        cout << "File reading error" << endl;
+    }
+    // read every line of file
+    string line;
+    getline(file, line); // First line
+    while (getline(file, line)) { // Starts on second line of file
+        stringstream string_stream(line);
+        string rank_s;
+        int rank;
+        string word;
+        getline(string_stream, rank_s, ',');
+        getline(string_stream, word, ',');
+        rank = stoi(rank_s);
+        // insert
+        trie.insert(rank, word);
+        // (HASH INSERT HERE) hash.insert(rank, word); ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    }
 
     Screen screen;
     screen.openWindow(800.0f, 600.0f);
